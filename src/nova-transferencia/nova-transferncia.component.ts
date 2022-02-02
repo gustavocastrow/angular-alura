717e5d8aa951cbd50d72dd252f30f7e4a32cf4d1
Gustavo Castro <gustavocastrocs@gmail.com>
2022-02-02 09:29:47 -0300

    Modulo 02

--- src/nova-transferencia/nova-transferncia.component.ts
+++ src/nova-transferencia/nova-transferncia.component.ts
@@ -8,8 +8,14 @@ import { Component } from "@angular/core";
 })
 
 export class NovaTransferenciaComponent {
+
+  valor: number;
+  destino: number;
+
   transferir(){
     console.log("Solicitada nova transferência")
+    console.log(`Valor: ${this.valor}`)
+    console.log(`Destino: ${this.destino}`)
   }
 
 }
